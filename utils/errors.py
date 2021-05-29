2e225d4401d08cc90c697f81caf7817bece7c7a5
TheLeadingLlama <44980366+StockerMC@users.noreply.github.com>
2021-05-29 19:50:57 -0400

    Removed unused exceptions

--- utils/errors.py
+++ utils/errors.py
@@ -1,15 +1,8 @@
 from discord.ext.commands import CommandError
 
-# class EmojiHelperException(CommandError):
-# 	"""The base class for all errors"""
-# 	pass
-
 class EmojifyDisabled(Exception):
 	"""The emojify command is disabled"""
 	pass
-
-# class InvalidURL(Exception): # replaced with assertionerror
-# 	"""The URL provided was invalid"""
 
 class URLNotImage(Exception):
 	"""The URL provided was not an image""" # or not a valid image?
