16d487798d69083ba3c57b02dba667ccfbf4be94
TheLeadingLlama <44980366+StockerMC@users.noreply.github.com>
2021-05-30 15:10:02 -0400

    Debug print

--- bot.py
+++ bot.py
@@ -44,6 +44,7 @@ bot.help_command = Help(sort_commands=True)
 
 @bot.event
 async def on_commnad(ctx):
+	print(1)
 	embed = discord.Embed(title=f"`{ctx.command}`", color=bot.color)
 	embed.add_field(name="Author", value=f"{ctx.author} ({ctx.author.id})")
 	embed.add_field(name="Guild", value=f"{ctx.guild} ({ctx.guild.id})")
