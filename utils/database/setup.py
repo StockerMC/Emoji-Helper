6efba5e09a7da7b4b9d2e30721a7eb369c53623b
TheLeadingLlama <44980366+StockerMC@users.noreply.github.com>
2021-06-07 13:34:32 -0400

    Typing

--- utils/database/setup.py
+++ utils/database/setup.py
@@ -3,7 +3,7 @@ from typing import Any
 
 async def setup(config: dict[str, Any]):
 	del config["database"]["setup_completed"]
-	postgres_config = config["database"]
+	postgres_config: dict[str, Any] = config["database"]
 	async with asyncpg.create_pool(**postgres_config) as pool:
 		async with pool.acquire() as con:
 			async with con.transaction():
